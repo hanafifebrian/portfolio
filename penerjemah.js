19e624d6f1d8d49f13207ecc04f0e606a3601fc4
hanafifebrian_22 <hanafifebrian87@gmail.com>
2023-12-24 13:14:24 -0800

    add language

---
+++ penerjemah.js
@@ -0,0 +1,319 @@
+const languageData = {
+    'id': {
+        // nav
+        'title':'Indonesia',
+        'title1':'English',
+        'title2':'Spanyol',
+        'title3':'Arab',
+        'title4':'Jepang',
+        'title5':'Korea',
+        'namleng':'Hanafi febriansyah',
+        'home': 'beranda',
+        'about': 'tentang',
+        'portp':'portofolio',
+        'contact': 'kontak',
+        'gelter':'Gelap & Terang',
+        'ubah':'Ubah bahasa',
+        // change
+        'language':'Pilih bahasa',
+        'close':'Tutup',
+        // home
+        'greeting': 'Halo semua 👋, perkenalkan saya',
+        'introduction': 'saya adalah seorang pelajar',
+        'journey': 'Semoga perjalanan Saya dalam dunia pemrograman terus berkembang dan membawa kesuksesan di masa depan!',
+        'contact1': 'hubungi saya',
+        // about
+        'about-me': 'Tentang saya',
+        'journey1': 'Perjalanan',
+        'birthplace': 'Saya lahir di Semarang tahun 2007, besar di desa Sruwen. Menemukan ketertarikan pada pemrograman saat kelas 8 SMP. Sejak itu, Saya terus eksplorasi dunia pemrograman hingga saat ini.',
+        'social-media': 'Media sosial',
+        'social-media-description': 'Berikut media sosial yang saya gunakan untuk sehari-hari',
+        'experience': 'Pengalaman',
+        'experience-description': 'Saya telah menggali beberapa bahasa pemrograman seperti HTML, CSS, Tailwind, Bootstrap, Python, JavaScript, dan PHP.',
+        'learned': 'Yang saya pelajari',
+        'learned-description': 'Beberapa Bahasa yang lebih sering saya pelajari selama ini',
+        'journey2': 'Sekolah',
+        'journey3': 'Saya memulai pendidikan di MI Sruwen 04. Kemudian, melanjutkan pendidikan di SMP N 1 Tengaran dan melanjutkan ke SMK N1 Tengaran Selain pendidikan formal, Saya juga mengikuti program non formal yaitu Karir Anak.',
+        'school': 'Pernah belajar di',
+        // portofolio
+        'portf':'Proyek Saya',
+        // contact
+        'name':'Nama',
+        'message':'Pesan',
+        'send':'Kirim',
+        // footer
+        'made': 'Dibuat oleh',
+        'madeW':'Dibuat Dengan',
+        'link':'Tautan',
+      },
+
+      'en': {
+        // nav
+        'title': 'Indonesia',
+        'title1': 'English',
+        'title2': 'Spanish',
+        'title3': 'Arabic',
+        'title4': 'Japanese',
+        'title5': 'Korean',
+        'namleng': 'Hanafi Febriansyah',
+        'home': 'home page',
+        'about': 'about',
+        'portp':'portfolio',
+        'contact': 'contact',
+        'gelter':'Night & Bright',
+        'ubah':'Change language',
+        // change
+        'language':'select a language',
+        'close':'Close',
+        // home
+        'greeting': 'Hello everyone 👋, let me introduce myself',
+        'introduction': 'I am a student',
+        'journey': 'I hope my journey in the programming world continues to grow and brings success in the future!',
+        'contact1': 'Contact me',
+        // about
+        'about-me': 'About me',
+        'journey1': 'Journey',
+        'birthplace': 'I was born in Semarang in 2007, raised in the village of Sruwen. Discovered an interest in programming in 8th grade of junior high school. Since then, I have been exploring the world of programming.',
+        'social-media': 'Social Media',
+        'social-media-description': 'Here are the social media platforms I use on a daily basis',
+        'experience': 'Experience',
+        'experience-description': 'I have explored various programming languages such as HTML, CSS, Tailwind, Bootstrap, Python, JavaScript, and PHP.',
+        'learned': "What I've Learned",
+        'learned-description': 'Some languages that I have focused on learning include',
+        'journey2': 'School',
+        'journey3': "I started my education at MI Sruwen 04. Then, I continued my education at SMP N 1 Tengaran and continued to SMK N1 Tengaran. Apart from formal education, I also took part in a non-formal program, namely Children's Career.",
+        'school': 'Ever studied at',
+        // portofolio
+        'portf':'My Project',
+        // contact
+        'name':'Name',
+        'message':'Message',
+        'send':'Send',
+        // footer
+        'made': 'Made by',
+        'madeW':'Made With',
+        'link':'Link',
+      },
+      
+      'es': {
+        // nav
+        'title': 'Indonesia',
+        'title1': 'Inglés',
+        'title2': 'Español',
+        'title3': 'Árabe',
+        'title4': 'Japonés',
+        'title5': 'Coreano',
+        'namleng': 'Hanafi Febriansyah',
+        'home': 'página de inicio',
+        'about': 'sobre',
+        'portp': 'portafolio',
+        'contact': 'contacto',
+        'gelter': 'Noche y Brillante',
+        'ubah': 'Cambiar idioma',
+        // change
+        'language': 'seleccionar un idioma',
+        'close': 'Cerrar',
+        // home
+        'greeting': '¡Hola a todos 👋, permítanme presentarme!',
+        'introduction': 'Soy un estudiante',
+        'journey': '¡Espero que mi trayectoria en el mundo de la programación continúe creciendo y traiga éxito en el futuro!',
+        'contact1': 'Contáctame',
+        // about
+        'about-me': 'Sobre mí',
+        'journey1': 'Trayectoria',
+        'birthplace': 'Nací en Semarang en 2007, crecí en el pueblo de Sruwen. Descubrí un interés en la programación en el octavo grado de la escuela secundaria. Desde entonces, he estado explorando el mundo de la programación.',
+        'social-media': 'Redes sociales',
+        'social-media-description': 'Aquí están las plataformas de redes sociales que uso a diario',
+        'experience': 'Experiencia',
+        'experience-description': 'He explorado varios lenguajes de programación como HTML, CSS, Tailwind, Bootstrap, Python, JavaScript y PHP.',
+        'learned': 'Lo que he aprendido',
+        'learned-description': 'Algunos de los idiomas en los que me he centrado en aprender incluyen',
+        'journey2': 'Escuela',
+        'journey3': 'Comencé mi educación en MI Sruwen 04. Luego, continué mi educación en SMP N 1 Tengaran y continué en SMK N1 Tengaran. Aparte de la educación formal, también participé en un programa no formal, llamado Carrera Infantil.',
+        'school': 'Estudié en',
+        // portofolio
+        'portf': 'Mis proyectos',
+        // contact
+        'name': 'Nombre',
+        'message': 'Mensaje',
+        'send': 'Enviar',
+        // footer
+        'made': 'Hecho por',
+        'madeW': 'Hecho con',
+        'link': 'Enlace',
+    },
+    
+    'ar': {
+        // nav
+        'title': 'إندونيسيا',
+        'title1': 'الإنجليزية',
+        'title2': 'الإسبانية',
+        'title3': 'العربية',
+        'title4': 'اليابانية',
+        'title5': 'الكورية',
+        'namleng': 'حنفي فبريانشاه',
+        'home': 'الصفحة الرئيسية',
+        'about': 'حول',
+        'portp': 'المحفظة',
+        'contact': 'اتصل',
+        'gelter': 'ليل وسطوع',
+        'ubah': 'تغيير اللغة',
+        // change
+        'language': 'اختر لغة',
+        'close': 'إغلاق',
+        // home
+        'greeting': 'مرحبًا بالجميع 👋، دعوني أقدم نفسي',
+        'introduction': 'أنا طالب',
+        'journey': 'آمل أن تستمر رحلتي في عالم البرمجة في النمو وتحقيق النجاح في المستقبل!',
+        'contact1': 'تواصل معي',
+        // about
+        'about-me': 'عني',
+        'journey1': 'الرحلة',
+        'birthplace': 'ولدت في سمرانج في عام 2007، نشأت في قرية سروين. اكتشفت اهتمامًا بالبرمجة في الصف الثامن من المدرسة الثانوية. منذ ذلك الحين، بدأت استكشاف عالم البرمجة.',
+        'social-media': 'وسائل التواصل الاجتماعي',
+        'social-media-description': 'إليك منصات وسائل التواصل الاجتماعي التي أستخدمها يوميًا',
+        'experience': 'الخبرة',
+        'experience-description': 'لقد استكشفت لغات برمجة متنوعة مثل HTML و CSS و Tailwind و Bootstrap و Python و JavaScript و PHP.',
+        'learned': 'ما تعلمته',
+        'learned-description': 'بعض اللغات التي ركزت على تعلمها تشمل',
+        'journey2': 'المدرسة',
+        'journey3': 'بدأت تعليمي في مدرسة MI Sruwen 04. ثم، واصلت تعليمي في SMP N 1 Tengaran وواصلت في SMK N1 Tengaran. بالإضافة إلى التعليم الرسمي، شاركت أيضًا في برنامج غير رسمي، وهو برنامج مهنة الأطفال.',
+        'school': 'درست في',
+        // portofolio
+        'portf': 'مشروعاتي',
+        // contact
+        'name': 'الاسم',
+        'message': 'رسالة',
+        'send': 'إرسال',
+        // footer
+        'made': 'صنع بواسطة',
+        'madeW': 'صنع بواسطة',
+        'link': 'رابط',
+    },
+    
+    'ja': {
+        // nav
+        'title': 'インドネシア',
+        'title1': '英語',
+        'title2': 'スペイン語',
+        'title3': 'アラビア語',
+        'title4': '日本語',
+        'title5': '韓国語',
+        'namleng': 'ハナフィ・フェブリアンシャ',
+        'home': 'ホームページ',
+        'about': '約',
+        'portp': 'ポートフォリオ',
+        'contact': 'お問い合わせ',
+        'gelter': 'ナイト＆ブライト',
+        'ubah': '言語を変更',
+        // change
+        'language': '言語を選択',
+        'close': '閉じる',
+        // home
+        'greeting': 'みなさん、こんにちは 👋、自己紹介させてください',
+        'introduction': '私は学生です',
+        'journey': 'プログラミングの世界での私の旅が成長し、将来に成功をもたらすことを願っています！',
+        'contact1': 'お問い合わせ',
+        // about
+        'about-me': '私について',
+        'journey1': '旅',
+        'birthplace': '私は2007年にセマランで生まれ、スルウェンの村で育ちました。中学校の8年生でプログラミングへの興味を見つけました。それ以来、プログラミングの世界を探求しています。',
+        'social-media': 'ソーシャルメディア',
+        'social-media-description': '私が日常的に使用しているソーシャルメディアプラットフォームです',
+        'experience': '経験',
+        'experience-description': '私はHTML、CSS、Tailwind、Bootstrap、Python、JavaScript、PHPなどさまざまなプログラミング言語を探求してきました。',
+        'learned': '私が学んだこと',
+        'learned-description': '私が重点的に学んだいくつかの言語には',
+        'journey2': '学校',
+        'journey3': 'MI Sruwen 04で教育を受け始めました。その後、SMP N 1 Tengaranで教育を続け、SMK N1 Tengaranに進学しました。正規の教育のほかに、子供のキャリアという非正規のプログラムにも参加しました。',
+        'school': '学んだことがあります',
+        // portofolio
+        'portf': '私のプロジェクト',
+        // contact
+        'name': '名前',
+        'message': 'メッセージ',
+        'send': '送信',
+        // footer
+        'made': '制作',
+        'madeW': '作成者',
+        'link': 'リンク',
+    },
+    
+    'ko': {
+        // nav
+        'title': '인도네시아',
+        'title1': '영어',
+        'title2': '스페인어',
+        'title3': '아랍어',
+        'title4': '일본어',
+        'title5': '한국어',
+        'namleng': '하나피 페브리안샤',
+        'home': '홈페이지',
+        'about': '소개',
+        'portp': '포트폴리오',
+        'contact': '문의',
+        'gelter': '나잇 앤 브라이트',
+        'ubah': '언어 변경',
+        // change
+        'language': '언어 선택',
+        'close': '닫기',
+        // home
+        'greeting': '안녕하세요 모두 👋, 자기 소개를 할게요',
+        'introduction': '저는 학생입니다',
+        'journey': '프로그래밍 세계에서의 나의 여정이 계속 성장하고 미래에 성공을 가져다 주길 바라요!',
+        'contact1': '문의하기',
+        // about
+        'about-me': '나에 관해',
+        'journey1': '여정',
+        'birthplace': '나는 2007년 세마랑에서 태어났고, 스르웬 마을에서 자랐어요. 중학교 8학년 때 프로그래밍에 흥미를 느꼈고, 그때부터 프로그래밍 세계를 탐험해 왔어요.',
+        'social-media': '소셜 미디어',
+        'social-media-description': '나는 일상에서 사용하는 소셜 미디어 플랫폼입니다',
+        'experience': '경험',
+        'experience-description': 'HTML, CSS, Tailwind, Bootstrap, Python, JavaScript, PHP 등 다양한 프로그래밍 언어를 탐험해 왔어요.',
+        'learned': '내가 배운 것',
+        'learned-description': '주로 배우고 있는 몇 가지 언어에는',
+        'journey2': '학교',
+        'journey3': 'MI Sruwen 04에서 교육을 시작했고, 그 후 SMP N 1 Tengaran에서 교육을 계속하고 SMK N1 Tengaran으로 진학했어요. 공식 교육 외에도 어린이 경력이라는 비정규 프로그램에 참여했어요.',
+        'school': '다니었던 학교',
+        // portofolio
+        'portf': '내 프로젝트',
+        // contact
+        'name': '이름',
+        'message': '메시지',
+        'send': '보내기',
+        // footer
+        'made': '제작자',
+        'madeW': '제작됨',
+        'link': '링크',
+    },
+    
+  };
+
+  function changeLanguage(language) {
+    // Dapatkan teks untuk bahasa yang dipilih
+    const texts = languageData[language];
+
+    // Perbarui seluruh konten dengan teks baru
+    Object.keys(texts).forEach((key) => {
+      const elements = document.querySelectorAll(`.lang-${key}`);
+      elements.forEach((element) => {
+        element.textContent = texts[key];
+      });
+    });
+
+    // Tutup modal setelah mengubah bahasa
+    const modal = document.getElementById('languageModal');
+    modal.classList.add('invisible');
+  }
+
+  const languageSelectorButton = document.getElementById('languageSelectorButton');
+  const languageModal = document.getElementById('languageModal');
+  const closeModalButton = document.getElementById('closeModalButton');
+
+  languageSelectorButton.addEventListener('click', () => {
+    languageModal.classList.remove('invisible');
+  });
+
+  closeModalButton.addEventListener('click', () => {
+    languageModal.classList.add('invisible');
+  });
